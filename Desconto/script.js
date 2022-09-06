a56b2a97d0251eaa40b3d9308c03d9ab10309862
Danyel <danyeljornooki@hotmail.com>
2022-09-06 16:06:07 -0300

    att

---
+++ Desconto/script.js
@@ -0,0 +1,20 @@
+function calcular(num1, num2) {
+  var divR = document.getElementById("resultado");
+  var num1 = document.getElementById("a").value;
+  var num2 = document.getElementById("b").value;
+  var p;
+  var result = contas(num1, num2);
+  if (num1 != "" && num2 != "") {
+    p = document.createElement("p");
+    p.append(result);
+    divR.appendChild(p);
+  } else {
+    console.log('vazio');
+  }
+}
+
+function contas(num1, num2) {
+total = num1 -((num1 * num2) / 100).toFixed(1);
+
+  return (result = "O novo valor é: R$" + total);
+}
